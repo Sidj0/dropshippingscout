eda53a8b92b11a954139a3e6e70a1ea7c41eca9c
MahmoudHassan7 <43067089+MahmoudHassan7@users.noreply.github.com>
2024-11-27 15:31:56 +0400

    Update tool.blade.php

--- resources/views/tool.blade.php
+++ resources/views/tool.blade.php
@@ -6,17 +6,11 @@
 @section('meta_keywords', $page->meta_keywords)
 @section('meta_author', $page->meta_author)
 
+@section('og_title', $page->content_header)
+@section('og_description', $page->content_subheader)
+@section('og_image', asset('images/' . $page->image_1))
 
-
-@section('meta')
-    <!-- Meta -->
-    <meta property="og:title" content="{{ $page->content_header }}" />
-    <meta property="og:description" content="{{ $page->content_subheader }}" />
-    <meta property="og:image" content="{{ asset('images/' . $page->image_1) }}" />
-    <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="DropShippingScout" />
-@endsection
+ 
 
 @section('styles')
     <!-- Custom CSS for this view -->
