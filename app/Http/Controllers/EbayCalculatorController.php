d530f10db16b7523c7fc14b0f84d18ec00a36cfe
magdyR <developer.eye1@gmail.com>
2024-12-06 14:27:06 +0200

    ff

--- app/Http/Controllers/EbayCalculatorController.php
+++ app/Http/Controllers/EbayCalculatorController.php
@@ -57,6 +57,7 @@ class EbayCalculatorController extends Controller
         $response = Http::withHeaders([
             'api-key' => '1d95bfb7-b38a-50e4-b5f9-cb348deb4021'
        ])->post("http://164.90.165.80/shopify-api/public/index.php/api/ebay-calculator?".implode("&",$ar));
+       dd($response);
        $result=  $response->json() ;
 
        // $request->all() to object
