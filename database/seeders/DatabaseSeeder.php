ad699f387ec726b933f6273be28e9b736697ffe1
MagdyR <developer.eye1@gmail.com>
2025-10-29 15:23:49 +0300

    Add initial data seeding for FAQs, pages, tools, and users
    - Created faqs.csv with 21 entries covering tool features, support assistance, and pricing subscriptions.
    - Added pages.csv with 15 entries for various site pages including About, Contact, and Privacy Policy.
    - Introduced tools.csv with 15 entries detailing product research tools for Amazon, eBay, Shopify, and TikTok.
    - Seeded users.csv with 4 user accounts, including admin and regular users.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
-
 namespace Database\Seeders;
-
+use App\Models\Blog;
+use App\Models\Faq;
+use App\Models\Page;
+use App\Models\tool;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->importCSV('users', new User());
+        $this->importCSV('tools', new tool());
+        $this->importCSV('pages', new Page());
+        $this->importCSV('faqs', new Faq());
+        $this->importCSV('blogs', new Blog());
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    }
+
+    private function importCSV($file, $model) {
+        $csv = fopen(__DIR__."/{$file}.csv", 'r');
+        $data = [];
+        $headers = fgetcsv($csv, 1000, ",");
+
+        printf("*Start import `{$file}.csv`\n");
+
+        $i=0;
+        while (($row = fgetcsv($csv, 1000000,",")) !== FALSE) {
+            $data=[];
+            $j=0;
+            foreach ( $headers as $header) {
+                $data[$header]= $row[$j];
+                $j++;
+            }
+            $model->create($data);
+            $i++;
+        }
+
+        fclose($csv);
+        printf("=imported `{$file}` ({$i}) rows\n\n");
     }
 }
